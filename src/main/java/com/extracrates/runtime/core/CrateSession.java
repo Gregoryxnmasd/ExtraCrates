1cb63d82b043a21471d974cedcde2073acddc74e
Palomero33 <101456667+Gregoryxnmasd@users.noreply.github.com>
2026-01-16 19:11:56 +0200

    Add missing ArmorStand import

--- src/main/java/com/extracrates/runtime/core/CrateSession.java
+++ src/main/java/com/extracrates/runtime/core/CrateSession.java
@@ -14,6 +14,7 @@ import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemDisplay;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.TextDisplay;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
