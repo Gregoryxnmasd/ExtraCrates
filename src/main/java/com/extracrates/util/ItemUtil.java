c22844d5eca55ed0d64e886c879a1b0f1daa2750
Palomero33 <101456667+Gregoryxnmasd@users.noreply.github.com>
2026-01-16 23:22:44 +0200

    Unify config package imports (#80)

--- src/main/java/com/extracrates/util/ItemUtil.java
+++ src/main/java/com/extracrates/util/ItemUtil.java
@@ -1,7 +1,7 @@
 package com.extracrates.util;
 
-import com.extracrates.config.SettingsSnapshot;
 import com.extracrates.config.ConfigLoader;
+import com.extracrates.config.SettingsSnapshot;
 import com.extracrates.model.Reward;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
