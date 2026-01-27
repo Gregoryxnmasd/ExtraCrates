d2b4f763cafc15121772cf59c6e9ab762a5892a1
Palomero33 <101456667+Gregoryxnmasd@users.noreply.github.com>
2026-01-27 13:44:21 +0200

    Fix Locale import in CrateSession (#311)

--- src/main/java/com/extracrates/runtime/core/CrateSession.java
+++ src/main/java/com/extracrates/runtime/core/CrateSession.java
@@ -39,6 +39,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
